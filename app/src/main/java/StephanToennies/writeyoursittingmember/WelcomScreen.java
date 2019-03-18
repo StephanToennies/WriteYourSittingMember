7b9e95053df12d3232d8804bca596017513c58b7
Stephan <Stephan.Toennies@gmail.com>
2019-03-18 12:50:40 +0100

    v0.007 add switch to activities

--- app/src/main/java/StephanToennies/writeyoursittingmember/WelcomScreen.java
+++ app/src/main/java/StephanToennies/writeyoursittingmember/WelcomScreen.java
@@ -2,12 +2,33 @@ package StephanToennies.writeyoursittingmember;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+
+import StephanToennies.writeyoursittingmember.MainActivity;
 
 public class WelcomScreen extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcom_screen);
+
+        button = (Button) findViewById(R.id.buttonStartMainActivity);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
     }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
