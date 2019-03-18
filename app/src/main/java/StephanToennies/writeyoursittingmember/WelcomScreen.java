fa13dffe2e05133a9e0c14c69c2a7e9ebae63e1e
Stephan <Stephan.Toennies@gmail.com>
2019-03-18 11:52:00 +0100

    add new activity

---
+++ app/src/main/java/StephanToennies/writeyoursittingmember/WelcomScreen.java
@@ -0,0 +1,13 @@
+package StephanToennies.writeyoursittingmember;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom_screen);
+    }
+}
