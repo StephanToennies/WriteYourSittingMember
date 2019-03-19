70ad4c20cf4a278c06a59a5a8ad79c422f01b4b6
Stephan <Stephan.Toennies@gmail.com>
2019-03-19 12:08:49 +0100

    delite FAP
    FAP is not supported in this version

--- app/src/main/java/StephanToennies/writeyoursittingmember/MainActivity.java
+++ app/src/main/java/StephanToennies/writeyoursittingmember/MainActivity.java
@@ -2,27 +2,18 @@ package StephanToennies.writeyoursittingmember;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.support.design.widget.Snackbar;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab = findViewById(R.id.fab);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
